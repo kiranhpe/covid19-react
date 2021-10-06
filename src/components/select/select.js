d6c069ab1b47e0a50449e625a62b8c3ae204e38b
kirantatkal1 <kirantatkal@gmail.com>
2021-10-06 16:59:41 +0530

    css fix for select

--- src/components/select/select.js
+++ src/components/select/select.js
@@ -7,6 +7,20 @@ export const DropDown =({onStateChange,data,isLoading,placeholder})=> {
     setSelectedOption(selectedItem);
     onStateChange(selectedItem);
   };
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      borderBottom: '1px dotted pink',
+      color: state.isSelected ? 'red' : 'blue',
+    }),
+    singleValue: (provided, state) => {
+      const opacity = state.isDisabled ? 0.5 : 1;
+      const transition = 'opacity 300ms';
+  
+      return { ...provided, opacity, transition };
+    }
+  }
+
   return (
     <div className="select">
       <Select
@@ -14,7 +28,7 @@ export const DropDown =({onStateChange,data,isLoading,placeholder})=> {
         onChange={handleChange}
         options={data}
         placeholder={"Select " + placeholder}
-        isLoading={isLoading}
+        isLoading={isLoading} styles={customStyles}
       />
     </div>
   );
