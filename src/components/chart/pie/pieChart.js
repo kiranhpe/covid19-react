32992d73b09e2d0b7a5452edb92319768655c5cf
kirantatkal1 <kirantatkal@gmail.com>
2021-10-07 21:25:37 +0530

    optimize

--- src/components/chart/pie/pieChart.js
+++ src/components/chart/pie/pieChart.js
@@ -1,13 +1,8 @@
 import React from "react";
-import { PieChart, Pie, Cell, Legend, Label } from "recharts";
+import { PieChart, Pie, Cell, Legend } from "recharts";
 
 export const CVPieChart = ({ pieData }) => {
-  const data = [
-    { name: "Group A", value: 400 },
-    { name: "Group B", value: 300 },
-    { name: "Group C", value: 300 },
-    { name: "Group D", value: 200 },
-  ];
+
   const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
   return (
     <PieChart height={300} width={400}>
